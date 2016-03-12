3f705a48fb2775ef37849386390294d09f361ee2
Jonathan Gao <jonathan-gao@hotmail.com>
2016-03-12 17:46:13 -0500

    Optimized and less unnecessary memory allocations

--- imageProcessing.py
+++ imageProcessing.py
@@ -14,22 +14,25 @@ import pybrain.tools.shortcuts as pb
 #     dst = cv2.warpAffine(img,M,(cols,rows))
 
 
-class ImProcess(object):
+class Corner(object):
     # global img1, img2, grayimg1, grayimg2, cornerMat1, cornerMat2, temp1
-    def __init__(self, file1, file2, matchPoints=50):
-        self.matchPoints = matchPoints
-        self.img1 = file1  # cv2.imread(file1)
-        self.img2 = file2  # cv2.imread(file2)
-        self.grayimg1 = self.initiateimage(self.img1)
-        self.grayimg2 = self.initiateimage(self.img2)
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        # self.matchPoints = matchPoints
+        # self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+        # # self.img1 = file1  # cv2.imread(file1)
+        # # self.img2 = file2  # cv2.imread(file2)
+    def setImages(self, file1, file2):
+        self.grayimg1 = self.initiateimage(file1)
+        self.grayimg2 = self.initiateimage(file2)
 
     def initiateimage(self, image):
         if image is None:
             print 'Image is null!'
+            quit()
         else:
-            imgres = cv2.resize(image, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
-            return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # imgres = cv2.resize(image, None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
+            imggray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # imgcont = cv2.equalizeHist(imggray)
+            return imggray
 
     def FAST(self, img):
         fast = cv2.FastFeatureDetector_create()
@@ -85,9 +88,9 @@ class ImProcess(object):
         # matches = sorted(matches, key = lambda x:x.distance)
         good = []
         for m, n in matches:
-            if m.distance < 0.75 * n.distance:
+            if m.distance < 0.6 * n.distance:
                 good.append(m)
-        # matches = good
+        matches = good
         list_kp1 = []
         list_kp2 = []
         for m in good:
@@ -101,29 +104,57 @@ class ImProcess(object):
             return cv2.drawMatches(img1, kp1, img2, kp2, good[:self.matchPoints], None, flags=2)
         else:
             return list_kp1, list_kp2
-
-    def main(self):
-        cv2.imwrite('/home/jonathan/Documents/ImageProcessingImages/grayimg1.JPG', self.grayimg1)
-        cv2.imwrite('/home/jonathan/Documents/ImageProcessingImages/grayimg2.JPG', self.grayimg2)
+    def getImages(self):
+        return self.grayimg1, self.grayimg2
+    def getCorners(self):
+        return self.corner(self.grayimg1), self.corner(self.grayimg2)
+        # cv2.imwrite('/home/jonathan/Documents/ImageProcessingImages/grayimg1.JPG', self.grayimg1)
+        # cv2.imwrite('/home/jonathan/Documents/ImageProcessingImages/grayimg2.JPG', self.grayimg2)
         # self.cornerMat1 = self.corner(self.grayimg1)
         # self.cornerMat2 = self.corner(self.grayimg2)
         # mat1, mat2 = self.match(self.grayimg1, self.grayimg2, False)
-
+        #
         # print "Coordinate set 1 of ", len(mat1),": ", mat1
         # print "Coordinate set 2 of ", len(mat2),": ", mat2
-        cv2.imshow('img', self.match(self.grayimg1, self.grayimg2, True))
+        # cv2.imshow('img', self.match(self.grayimg1, self.grayimg2, True))
+    def reset(self):
+        self.grayimg1 = None
+        self.grayimg2 = None
 
-    # self.grayimg1[self.cornerMat1>0.01*self.cornerMat1.max()]=[50]
-    # self.grayimg2[self.cornerMat2>0.01*self.cornerMat2.max()]=[50]
-    # cv2.imshow('image1', temp1)
-    # cv2.imshow('image2', temp2)
-    # cv2.imshow('image2', self.i)
+class Matcher(object):
+    def __init__(self):
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+    def match(self, corner1, corner2, matchPoints, draw=False):
+        kp1, des1 = corner1
+        kp2, des2 = corner2
+        matches = self.bf.knnMatch(des1, des2, k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.5 * n.distance:
+                good.append(m)
+        list_kp1 = []
+        list_kp2 = []
+        for m in good:
+            img1_idx = m.queryIdx
+            img2_idx = m.trainIdx
+            (x1, y1) = kp1[img1_idx].pt
+            (x2, y2) = kp2[img2_idx].pt
+            list_kp1.append((x1, y1))
+            list_kp2.append((x2, y2))
+        if draw:
+            return kp1, kp2, good[:matchPoints], list_kp1, list_kp2
+        else:
+            return list_kp1, list_kp2
 
 
 class VideoStream(object):
     def __init__(self, capid):
         self.cap = cv2.VideoCapture(capid)
-        self.cap.open(capid)
+        self.cap.set(5,24)
+        self.cap.set(3, 640)
+        self.cap.set(4, 480)
+        # self.cap.open(capid)
 
     def getFrame(self):
         ret, frame = self.cap.read()
@@ -145,14 +176,27 @@ class NNet(object):
 class Main(object):
     Camera1 = VideoStream(1)
     Camera2 = VideoStream(0)
-    while (True):
-        file1 = Camera1.getFrame()  # '/home/jonathan/Documents/ImageProcessingImages/img1.JPG'
-        file2 = Camera2.getFrame()  # cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img2.JPG')
-        Thread1 = ImProcess(file1, file2, 50)
-        Thread1.main()
+    Matcher1 = Matcher()
+    Thread1 = Corner()
+    while(True):
+        file1 = Camera1.getFrame()
+        file2 = Camera2.getFrame()
+        Thread1.setImages(file1, file2)
+        corner1, corner2 = Thread1.getCorners()
+        grayimg1, grayimg2 = Thread1.getImages()
+        try:
+            # Matcher1.match(corner1, corner2, 50, True)
+            # mat1, mat2 = Matcher1.match(corner1, corner2, 50, False)
+            kp1, kp2, good, mat1, mat2 = Matcher1.match(corner1, corner2, 50, True)
+            print "Coordinate set 1 of ", len(mat1),": ", mat1
+            print "Coordinate set 2 of ", len(mat2),": ", mat2
+            cv2.imshow('img', cv2.drawMatches(grayimg1, kp1, grayimg2, kp2, good, None, flags=2))
+        except:
+            Thread1.reset()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             Camera1.release()
             Camera2.release()
+            Thread1.reset()
             cv2.destroyAllWindows()
             break
 
