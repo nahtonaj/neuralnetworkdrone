42cff7d22f44688b1350238919af8cd236c96a65
Jonathan Gao <jonathan-gao@hotmail.com>
2016-04-12 10:51:40 -0400

    Remapping Done

--- imageProcessing.py
+++ imageProcessing.py
@@ -9,7 +9,7 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import TanhLayer
 from pybrain.supervised.trainers import BackpropTrainer
-
+import cPickle as pickle
 
 
 
@@ -142,10 +142,14 @@ class Matcher(object):
             list_kp1.append((x1, y1))
             list_kp2.append((x2, y2))
         # print list_kp1
+        dim = range(matchPoints)
         if draw:
-            return kp1, kp2, good[:matchPoints], list_kp1, list_kp2
+            if matchPoints>len(good):
+                return np.asarray(kp1), np.asarray(kp2), np.asarray(good), np.asarray(list_kp1), np.asarray(list_kp2)
+            else:
+                return np.asarray(kp1)[dim], np.asarray(kp2)[dim], np.asarray(good)[dim], np.asarray(list_kp1)[dim], np.asarray(list_kp2)[dim]
         else:
-            return list_kp1, list_kp2
+            return list_kp1[dim], list_kp2[dim]
 
 class VideoStream(object):
     def __init__(self, capid):
@@ -163,15 +167,21 @@ class VideoStream(object):
         self.cap.release()
 
 class NNet(object):
-    def __init__(self):
-        self.net = buildNetwork(2, 6, 2, hiddenclass=TanhLayer, bias=True)
+    def __init__(self, inpNeurons, hiddenNeurons, outNeurons):
+        self.net = buildNetwork(inpNeurons, hiddenNeurons, outNeurons, hiddenclass=TanhLayer, bias=True)
         self.net.randomize()
         print self.net
-        self.ds = SupervisedDataSet(2,2)
+        self.ds = SupervisedDataSet(inpNeurons,outNeurons)
         self.trainer = BackpropTrainer(self.net, self.ds, learningrate = 0.01, momentum=0.99)
     def addTrainDS(self, data1, data2, max):
-        norm1 = self.normalize(data1,max)
-        norm2 = self.normalize(data2,max)
+        norm1 = self.normalize(data1, max)
+        # n1x, n1y = np.hsplit(norm1, 2)
+        # n1x = n1x.flatten()
+        # n1y = n1y.flatten()
+        norm2 = self.normalize(data2, max)
+        # n2x, n2y = np.hsplit(norm2, 2)
+        # n2x = n2x.flatten()
+        # n2y = n2y.flatten()
         # print 'Normalized set 1:', norm1
         # print 'Normalized set 2:', norm2
         for x in range(len(norm1)):
@@ -180,9 +190,10 @@ class NNet(object):
         print "Training"
         # print self.trainer.train()
         trndata, tstdata = self.ds.splitWithProportion(.1)
-        # self.trainer.trainUntilConvergence(verbose=True,
-        #                                    maxEpochs=100)
-        self.trainer.trainOnDataset(trndata,500)
+        self.trainer.trainUntilConvergence(verbose=True,
+                                           trainingData=trndata,
+                                           maxEpochs=1000)
+        # self.trainer.trainOnDataset(trndata,500)
         self.trainer.testOnData(tstdata, verbose= True)
 
     def activate(self, data):
@@ -227,9 +238,9 @@ class NNet(object):
         # print 'Normalized value: ', norm
         if len(mat.shape)>1:
             for i in range(mat.shape[0]):
-                out = self.net.activate(mat[0])
+                out = self.net.activate(norm[0])
         else:
-            out=self.net.activate(mat)
+            out=self.net.activate(norm)
         # print 'Normalized output: ', out
         denorm = self.denormalize(out,max)
         # print 'Denormalized: ', denorm
@@ -246,38 +257,64 @@ class NNet(object):
             # for val in norm:
             #     out.append(self.net.activate(val))
             # return np.asarray(self.denormalize(out, max))
-    def transformImage(self, mat1):
-        print 'Transform begin'
-        outImage = np.empty_like(mat1)
-        for x in range(mat1.shape[0]):
-            for y in range(mat1.shape[1]):
-                try:
-                    newxy = self.getOutput(np.asarray((x,y)),mat1.shape[:2])
-                    outImage[newxy[0]][newxy[1]] = mat1[x][y]
-                except:
-                    pass
-        print 'Transform Done'
-        return outImage
+    def getRemap(self, sizex, sizey):
+        print 'Mapping begin'
+
+        oldx = np.arange(sizex)
+        oldy = np.arange(sizey)
+        mapx = np.zeros(shape=[sizex, sizey], dtype=np.float32)
+        mapy = np.zeros(shape=[sizex, sizey], dtype=np.float32)
+
+        for x in oldx:
+            for y in oldy:
+                # mapx[x][y] = 2*x
+                # mapy[x][y] = .5*y
+                newx, newy = self.getOutput(np.asarray((x,y)), np.asarray((sizex, sizey)))
+                mapx[x][y] = newx
+                mapy[x][y] = newy
+        # newx, newy = self.getOutput(oldxy, np.asarray((sizex, sizey)))
+        print 'Mapping Done'
+        return mapx, mapy
+
+
+                # try:
+                #     newxy = self.getOutput(np.asarray((x,y)),mat1.shape[:2])
+                    # print 'Old:', (x,y), 'new coord', newxy
+                    # outImage[newxy[0]][newxy[1]] = mat1[x][y]
+                # except:
+                #     print 'Error'
+                #     pass
+
+
     # def
 
 if __name__ == "__main__":
 
     Camera1 = VideoStream(0)
     Camera2 = VideoStream(1)
-    run = 0
+    file1dir = '/home/jonathan/Documents/ImageProcessingImages/left2.jpg'
+    file2dir = '/home/jonathan/Documents/ImageProcessingImages/right2.jpg'
     Matcher1 = Matcher()
     Corner1 = Corner()
-    NNet1 = NNet()
-    while run==0:
+    NNet1 = NNet(2,6,2)
+    mapx = None
+    mapy = None
+    matchpoints = 50
+    resize = .5
+    while True:
+        m = raw_input("Select Task \n 1. Train\n 2. Run\n 3. Cameras\n 4. Test Module\n 5. Exit\n >>:")
+        if m == '0':
+            pass
         # Camera1.release()
         # Camera2.release()
-        m = raw_input("Select Task \n 1. Train\n 2. Run\n 3. Cameras\n 4. Test Module\n 5. Exit\n >>:")
         if m == '1':
+            matchpoints = int(raw_input("Number of Match Points: "))
+            resize = float(raw_input("Image scale: "))
             # while(True):
             # file1 = Camera1.getFrame()
             # file2 = Camera2.getFrame()
-            file1 = cv2.resize(cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img1.JPG'), None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
-            file2 = cv2.resize(cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img2.JPG'), None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
+            file1 = cv2.resize(cv2.imread(file1dir), None, fx=resize, fy=resize, interpolation=cv2.INTER_AREA)
+            file2 = cv2.resize(cv2.imread(file2dir), None, fx=resize, fy=resize, interpolation=cv2.INTER_AREA)
             if file1 is None or file2 is None:
                 print 'Camera Error: Failed to get image\nReleasing Cameras..'
                 Camera1.release()
@@ -290,12 +327,13 @@ if __name__ == "__main__":
             # try:
             # Matcher1.match(corner1, corner2, 50, True)
             # mat1, mat2 = Matcher1.match(corner1, corner2, 50, False)
-            kp1, kp2, good, mat1, mat2 = Matcher1.match(corner1, corner2, 50, True)
-            cv2.imshow('img', cv2.drawMatches(grayimg1, kp1, grayimg2, kp2, good, None, flags=2))
-            print "Coordinate set 1 of ", len(mat1),": ", mat1
-            print "Coordinate set 2 of ", len(mat2),": ", mat2
+            kp1, kp2, good, mat1, mat2 = Matcher1.match(corner1, corner2, matchpoints, True)
+            # cv2.imshow('img', cv2.drawMatches(grayimg1, kp1, grayimg2, kp2, good, None, flags=2))
+            print "Coordinate set 1 of ", len(mat1),": ", mat1, "\nCoordinate set 2 of ", len(mat2),": ", mat2
             NNet1.addTrainDS(mat1, mat2, (height, width))
             NNet1.train()
+            # pickle
+            mapx, mapy = NNet1.getRemap(height, width)
             # print NNet1.getOutput(mat1, (height, width))
             # except:
 
@@ -310,8 +348,8 @@ if __name__ == "__main__":
             while(True):
                 # file1 = Camera1.getFrame()
                 # file2 = Camera2.getFrame()
-                file1 = cv2.resize(cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img1.JPG'), None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
-                file2 = cv2.resize(cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img2.JPG'), None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
+                file1 = cv2.resize(cv2.imread(file1dir), None, fx=resize, fy=resize, interpolation=cv2.INTER_AREA)
+                file2 = cv2.resize(cv2.imread(file2dir), None, fx=resize, fy=resize, interpolation=cv2.INTER_AREA)
                 height, width = file1.shape[:2]
                 Corner1.setImages(file1, file2)
                 corner1, corner2 = Corner1.getCorners()
@@ -319,18 +357,21 @@ if __name__ == "__main__":
                 # try:
                 # Matcher1.match(corner1, corner2, 50, True)
                 # mat1, mat2 = Matcher1.match(corner1, corner2, 50, False)
-                kp1, kp2, good, mat1, mat2 = Matcher1.match(corner1, corner2, 50, True)
-                print "Coordinate set 1 of ", len(mat1),": \n", mat1
-                print "Coordinate set 2 of ", len(mat2),": \n", mat2
+                kp1, kp2, good, mat1, mat2 = Matcher1.match(corner1, corner2, matchpoints, True)
+                # print "Coordinate set 1 of ", len(mat1),": \n", mat1
+                # print "Coordinate set 2 of ", len(mat2),": \n", mat2
                 # NNet1.addTrainDS(mat1, mat2, (height, width))
                 # NNet1.train()
-                leftImageCoord = grayimg1
-                remap = NNet1.transformImage(leftImageCoord)
-
-                # remapCoord = np.asarray(dest, np.float32)
-                # remap=np.empty_like(grayimg1)
-                # remap = cv2.remap(src=grayimg1, dst=grayimg2, map1=leftImageCoord, map2=remapCoord, interpolation=cv2.INTER_NEAREST)
-                cv2.imshow('image', remap)
+                # newx, newy = NNet1.transformImage(width, height)
+                if mapx==None or mapy==None:
+                    print 'Please train network first'
+                    m = '0'
+                    break
+                else:
+                    remap = cv2.remap(grayimg1, mapy, mapx, cv2.INTER_NEAREST)
+                    cv2.imshow('image', remap)
+                    print 'Remapping...'
+                # print "remap: ", remap
                 # cv2.imshow('img', cv2.drawMatches(grayimg1, kp1, grayimg2, kp2, good, None, flags=2))
                 # except:
 
@@ -340,16 +381,16 @@ if __name__ == "__main__":
                     Camera2.release()
                     Corner1.reset()
                     cv2.destroyAllWindows()
-                    cv2.waitKey(1)
+                    cv2.waitKey(0)
                     m='0'
                     break
 
         if m=='3':
-           while(True):
+            while(True):
                 # file1 = Camera1.getFrame()
                 # file2 = Camera2.getFrame()
-                file1 = cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img1.JPG')
-                file2 = cv2.imread('/home/jonathan/Documents/ImageProcessingImages/img2.JPG')
+                file1 = cv2.imread(file1dir)
+                file2 = cv2.imread('/home/jonathan/Documents/ImageProcessingImages/imRung2.JPG')
                 height, width = file1.shape[:2]
                 Corner1.setImages(file1, file2)
                 corner1, corner2 = Corner1.getCorners()
@@ -364,20 +405,22 @@ if __name__ == "__main__":
                 # except:
 
                 #     Corner1.reset()
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                if cv2.waitKey(0) & 0xFF == ord('q'):
                     Camera1.release()
                     Camera2.release()
                     Corner1.reset()
                     cv2.destroyAllWindows()
-                    cv2.waitKey(1)
+                    cv2.waitKey(0)
                     m='0'
                     break
         if m=='4':
             # print NNet1.getOutput(np.asarray((1500,1200)), np.asarray((3448,2448)))
-            print NNet1.normalize((1,2),(6,6))
+            print NNet1.denormalize(NNet1.normalize((1,2),(6,6)),(6,6))
+            print NNet1.getOutput(np.asarray((1,2)),(6,6))
         if m=='5':
+            print 'Exiting'
             break
-
-        print 'Please select a valid choice'
-    # return
+        else:
+            print 'Please select a valid choice'
+            # return
 # <codecell>
